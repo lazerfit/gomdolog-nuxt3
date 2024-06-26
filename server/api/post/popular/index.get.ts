eb3107eaa5d7ab9d8dcca64c3e730eb4615780ec
sg kim <redkafe98@gmail.com>
2024-06-26 21:24:53 +0900

    refactor: modify TheBanner to LazyTheBanner

--- server/api/post/popular/index.get.ts
+++ server/api/post/popular/index.get.ts
@@ -2,7 +2,7 @@ import type { PostResopnseWithoutTags } from '~/types';
 export default defineEventHandler(async (event) => {
 	const config = useRuntimeConfig();
 	const query = getQuery(event);
-	console.log(query);
+
 	const data: PostResopnseWithoutTags[] = await $fetch(
 		`${config.public.apiBase}/post/popular?limit=${query.limit}`
 	);
