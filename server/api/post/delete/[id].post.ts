1ed4d39e7e3dd71909a4306225c4b168ae68a614
sg kim <redkafe98@gmail.com>
2024-07-13 02:24:01 +0900

    refactor: change delete method

--- server/api/post/delete/[id].post.ts
+++ server/api/post/delete/[id].post.ts
@@ -1,18 +1,14 @@
-import { useRouter } from "vue-router";
-
 export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig();
-  const id = getRouterParam(event,'id');
-  const contentType = getHeader(event,'Content-Type') || 'application/json';
-  const authorization = getHeader(event,'Authorization') || '';
+	const config = useRuntimeConfig();
+	const id = getRouterParam(event, 'id');
+	const contentType = getHeader(event, 'Content-Type') || 'application/json';
+	const authorization = getHeader(event, 'Authorization') || '';
 
-  await $fetch(`${config.public.apiBase}/post/delete/${id}`, {
-    method: 'POST',
-    headers: {
-      'Authorization': authorization,
-      'Content-Type': contentType
-    }
-  }).then(() => {
-    useRouter().push('/');
-  }).catch(error => console.log(error));
+	await $fetch(`${config.public.apiBase}/post/delete/${id}`, {
+		method: 'POST',
+		headers: {
+			Authorization: authorization,
+			'Content-Type': contentType,
+		},
+	});
 });
